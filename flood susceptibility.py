0279c8afa4c9649c90d634321d7c707e03caa22b
Hayasakura <156915817+Hayasakura@users.noreply.github.com>
2026-04-02 16:20:29 +0800

    Update flood susceptibility.py

--- flood susceptibility.py
+++ flood susceptibility.py
@@ -664,7 +664,7 @@ feature_names = ['Elevation', 'Slope', 'Aspect', 'Curvature', 'Soil_Type', 'NDVI
                   'STI', 'Distance_to_Water', 'Lithology', 'Rainfall']
 
 # Input DBF path
-dbfroad = os.path.join(current_dir, 'sample', 'Chongqing_sample.dbf')  # SET INPUT PATH HERE
+dbfroad = os.path.join(current_dir, 'sample', 'anhui_sample.dbf')  # SET INPUT PATH HERE
 
 # Selected column indices (The last field must be the disaster label)
 factor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -673,7 +673,7 @@ output_dir = os.path.join(current_dir, 'output')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-yfxroad = os.path.join(output_dir, 'Chongqing_sample.dbf')
+yfxroad = os.path.join(output_dir, 'anhui_sample.dbf')
 
 # --------------------------- EXECUTION -------------------------------- #
 
